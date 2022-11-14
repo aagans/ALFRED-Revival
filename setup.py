51e4be8f7e6740922ae42b7378dc713414524354
aagans <aagans@bowdoin.edu>
2022-11-14 12:09:35 -0500

    Version 1.0 of ALFRED Revival
    Initial commit for GUI interface for the ALFRED Revival project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ALFRED_Revival',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Aale J. Agans',
+    author_email='aagans@bowdoin.edu',
+    description=''
+)
